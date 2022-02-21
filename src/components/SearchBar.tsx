d211f47d8ace2fb306cd84b27ea2f8cd2441da62
lazy-sky <1cielo@naver.com>
2022-02-21 19:45:30 +0900

    feat: Make SearchBar

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
+import useInput from '../hooks/useInput';
+import mixins from '../styles/mixins';
+
+const SearchContainer = styled.div`
+  position: relative;
+`;
+
+const SearchInput = styled.input`
+  ${mixins.fontStyle.body_07};
+  width: 100%;
+  margin-bottom: 32px;
+  padding: 12px 18px;
+  background-color: ${({ theme }) => theme.colors.blue_gray_02};
+  border: 1px solid ${({ theme }) => theme.colors.blue_03};
+  box-sizing: border-box;
+  border-radius: 40px;
+`;
+
+const SearchIcon = styled.div`
+  position: absolute;
+  top: 14px;
+  right: 18px; 
+  display: block;
+  width: 24px;
+  height: 24px;
+`;
+
+function SearchBar() {
+  const [keyword, handleKeyword] = useInput('');
+
+  return (
+    <SearchContainer>
+      <SearchInput
+        type="text"
+        placeholder="회사, 직무, 키워드 검색"
+        value={keyword}
+        onChange={handleKeyword}
+      />
+      <SearchIcon>
+        <img src="images/icon_magnifier.svg" alt="검색" />
+      </SearchIcon>
+    </SearchContainer>
+  );
+}
+
+export default SearchBar;
