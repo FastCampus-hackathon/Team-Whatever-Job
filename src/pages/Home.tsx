d737ddfc638ce36d6002ee82c5d1222fd368aead
lazy-sky <1cielo@naver.com>
2022-02-21 21:08:45 +0900

    feat: Markup Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,70 +1,105 @@
-import { useCallback, useState } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
+import Modal from '../components/Modal';
+import SearchBar from '../components/SearchBar';
+import { Container } from '../components/styled';
+import mixins from '../styles/mixins';
+
+const LoginButton = styled.button`
+  ${mixins.fontStyle.body_04};  
+  position: fixed;
+  top: 20px;
+  right: 24px;
+  padding: 8px 20px;
+  color: ${({ theme }) => theme.colors.white};
+  background-color: ${({ theme }) => theme.colors.blue_02};
+  border: none;
+  border-radius: 4px;
+
+  a {
+    text-decoration: none; 
+    color: ${({ theme }) => theme.colors.white}; 
+  }
+`;
+
+const LogoBox = styled.div`
+  margin-top: 40vh;
+  text-align: center;
+`;
+
+const Form = styled.form`
+  margin-top: 20px;
+`;
+
+const SearchCategories = styled.ul`
+  display: flex;
+  gap: 8px;
+  flex-wrap: nowrap;
+  overflow: auto;
+
+  li {
+    flex: 0 0 auto;
+  }
+`;
+
+const SearchCategory = styled.li`
+  ${mixins.fontStyle.body_08};
+  display: flex;
+  flex-wrap: nowrap;
+  gap: 6px; 
+  overflow: auto;
+  padding: 8px 12px;
+  border: 1px solid ${({ theme }) => theme.colors.blue_03};
+  box-sizing: border-box;
+  border-radius: 6px;
+`;
+
+const SearchCategoryName = styled.div`
+  color: ${({ theme }) => theme.colors.grayscale_02};
+`;
+
+const SearchCategorySelected = styled.div`
+  color: ${({ theme }) => theme.colors.blue_01};
+`;
 
 function Home() {
-  const [keyword, setKeyword] = useState('');
-
-  const handleInputChange = useCallback(
-    (e) => {
-      setKeyword(e.target.value);
-    }, []);
-
   return (
-    <div>
-      <div>
-        <button>
-          <Link to="/signin">로그인</Link>
-        </button>
-        <button>
-          <Link to="/signup">회원가입</Link>
-        </button>
-      </div>
-      <div>
-        로고
-      </div>
-      <form action="">
-        <input
-          type="text"
-          placeholder="회사, 직무, 키워드 검색"
-          value={keyword}
-          onChange={handleInputChange}
-        />
-        <div>
-          <div>
-            직무
-            <select name="" id="">
-              <option value="">디자인</option>
-              <option value="">개발</option>
-              <option value="">마케팅</option>
-            </select>
-          </div>
-          <div>
-            지역
-            <select name="" id="">
-              <option value="">서울</option>
-              <option value="">인천</option>
-              <option value="">경기도 </option>
-            </select>
-          </div>
-          <div>
-            고용형태
-            <select name="" id="">
-              <option value="">정규직</option>
-              <option value="">인턴</option>
-              <option value="">파견직</option>
-            </select>
-          </div>
-          <div>
-            정렬
-            <select name="" id="">
-              <option value="">최신순</option>
-              <option value="">인기순</option>
-            </select>
-          </div>
-        </div>
-      </form>
-    </div>
+    <Container>
+      <LoginButton>
+        <Link to="/signin">로그인</Link>
+      </LoginButton>
+      <LogoBox>
+        <img src="images/logo.png" alt="어구저구 로고" />
+      </LogoBox>
+      <Form>
+        <SearchBar />
+        <SearchCategories>
+          <SearchCategory>
+            <SearchCategoryName>직무</SearchCategoryName>
+            <SearchCategorySelected>선택</SearchCategorySelected>
+            <img src="images/icon_dropdown.svg" alt="선택" />
+          </SearchCategory>
+          <SearchCategory>
+            <SearchCategoryName>지역</SearchCategoryName>
+            <SearchCategorySelected>선택</SearchCategorySelected>
+            <img src="images/icon_dropdown.svg" alt="선택" />
+          </SearchCategory>
+          <SearchCategory>
+            <SearchCategoryName>고용 형태</SearchCategoryName>
+            <SearchCategorySelected>선택</SearchCategorySelected>
+            <img src="images/icon_dropdown.svg" alt="선택" />
+          </SearchCategory>
+          <SearchCategory>
+            <SearchCategoryName>정렬</SearchCategoryName>
+            <SearchCategorySelected>최신순</SearchCategorySelected>
+            <img src="images/icon_dropdown.svg" alt="선택" />
+          </SearchCategory>
+        </SearchCategories>
+
+      </Form>
+    </Container>
   );
 }
 
