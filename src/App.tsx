4fe2e1e56bdf8f35be01744690276ff7d07e5382
lazy-sky <1cielo@naver.com>
2022-02-21 19:07:43 +0900

    feat: Process routing in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 import { Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import { Reset } from 'styled-reset';
 
 import Home from './pages/Home';
 import MyPage from './pages/MyPage';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import theme from './styles/theme';
 
 function App() {
   return (
     <div className="App">
       <Reset />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/mypage" element={<MyPage />} />
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/mypage" element={<MyPage />} />
+        </Routes>
+      </ThemeProvider>
     </div>
   );
 }
