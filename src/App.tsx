e68fb27a905140507acabedf1bbfb0e71da95f47
lazy-sky <1cielo@naver.com>
2022-02-21 14:09:48 +0900

    feat: Set Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
+
+import Home from './pages/Home';
+import MyPage from './pages/MyPage';
+
 function App() {
   return (
     <div className="App">
-      Whatever
-      <div>
-        fdsfsdflkadsfjl
-        sdfasdf
-      </div>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/mypage" element={<MyPage />} />
+      </Routes>
     </div>
   );
 }
