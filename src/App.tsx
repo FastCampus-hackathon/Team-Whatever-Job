dd4f3e05e168e6a442870e20c683592b7e4e3c30
lazy-sky <1cielo@naver.com>
2022-02-21 15:53:35 +0900

    feat: Add routes in App

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { Reset } from 'styled-reset';
 
 import Home from './pages/Home';
 import MyPage from './pages/MyPage';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Reset />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
         <Route path="/mypage" element={<MyPage />} />
       </Routes>
     </div>
