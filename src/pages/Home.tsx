d651ae955d985c9904d126606a7459c3c8792e0f
lazy-sky <1cielo@naver.com>
2022-02-21 19:07:58 +0900

    feat: Fill contents in Home temporarily

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,69 @@
+import { useCallback, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+
 function Home() {
+  const [keyword, setKeyword] = useState('');
+
+  const handleInputChange = useCallback(
+    (e) => {
+      setKeyword(e.target.value);
+    }, []);
+
   return (
     <div>
-      Home
+      <div>
+        <button>
+          <Link to="/signin">로그인</Link>
+        </button>
+        <button>
+          <Link to="/signup">회원가입</Link>
+        </button>
+      </div>
+      <div>
+        로고
+      </div>
+      <form action="">
+        <input
+          type="text"
+          placeholder="회사, 직무, 키워드 검색"
+          value={keyword}
+          onChange={handleInputChange}
+        />
+        <div>
+          <div>
+            직무
+            <select name="" id="">
+              <option value="">디자인</option>
+              <option value="">개발</option>
+              <option value="">마케팅</option>
+            </select>
+          </div>
+          <div>
+            지역
+            <select name="" id="">
+              <option value="">서울</option>
+              <option value="">인천</option>
+              <option value="">경기도 </option>
+            </select>
+          </div>
+          <div>
+            고용형태
+            <select name="" id="">
+              <option value="">정규직</option>
+              <option value="">인턴</option>
+              <option value="">파견직</option>
+            </select>
+          </div>
+          <div>
+            정렬
+            <select name="" id="">
+              <option value="">최신순</option>
+              <option value="">인기순</option>
+            </select>
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
